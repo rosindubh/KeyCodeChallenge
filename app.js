69d32c43d1945c4247ff159c87a659f98f68842b
rosindubh <p.welsby2@ntlworld.com>
2021-08-11 01:00:09 +0100

    complete javascript and de-bug code

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 /*phil welsby 10 aug 2021 - key code challenge*/
+
+/*set variables*/
+let heading = document.getElementById('heading')
+let eventKey = document.getElementById('eventKey')
+let eventCode = document.getElementById('eventCode')
+let eventWhich = document.getElementById('eventWhich')
+let key = document.getElementById('key')
+
+/*main program*/
+document.addEventListener('keypress', () => {
+    heading.textContent = event.keyCode
+    eventKey.textContent = event.key
+    eventCode.textContent = event.code
+    eventWhich.textContent = event.charCode
+    console.dir(event)
+})
+
+
+
